1870f0724412b95687ac88bfd7cefbaaf7815e03
eden <eedenbaui@gmail.com>
2024-07-17 20:55:41 +0800

    chore: remove comments, delete unused component

--- src/helpers/serviceHelper.ts
+++ src/helpers/serviceHelper.ts
@@ -1,28 +1,26 @@
-import axios from 'axios'
+import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://api.escuelajs.co/api/v1/',
+    baseURL: "https://api.escuelajs.co/api/v1",
     timeout: 0,
-})
+});
 
-// Add a request interceptor
-axios.interceptors.request.use(function (config) {
-    // Do something before request is sent
-    return config;
-  }, function (error) {
-    // Do something with request error
-    return Promise.reject(error);
-  });
+axios.interceptors.request.use(
+    function (config) {
+        return config;
+    },
+    function (error) {
+        return Promise.reject(error);
+    }
+);
 
-// Add a response interceptor
-axios.interceptors.response.use(function (response) {
-    // Any status code that lie within the range of 2xx cause this function to trigger
-    // Do something with response data
-    return response;
-  }, function (error) {
-    // Any status codes that falls outside the range of 2xx cause this function to trigger
-    // Do something with response error
-    return Promise.reject(error);
-  });
+axios.interceptors.response.use(
+    function (response) {
+        return response;
+    },
+    function (error) {
+        return Promise.reject(error);
+    }
+);
 
-export default instance
+export default instance;
