64c88796626d83d545dbbe055c77bae7279a909c
eden <eedenbaui@gmail.com>
2024-07-17 21:00:10 +0800

    feat: add top products component in home

--- src/modules/home/views/Home.vue
+++ src/modules/home/views/Home.vue
@@ -1,20 +1,18 @@
 <template>
 
     <v-container class="my-10 d-flex flex-column ga-10">
-        <v-row id="topProducts">
-            <h2>Top Products</h2>
-            <v-col cols="12" lg="8">
-
-            </v-col>
-        </v-row>
+        <TopProducts id="topProducts" />
 
         <v-row id="dailyDiscover">
-            <h2>Daily Discover</h2>
+            <v-col cols="12">
+                <h2>Daily Discover</h2>
+            </v-col>
         </v-row>
     </v-container>
 </template>
 
 <script setup lang="ts">
+import TopProducts from '../components/TopProducts.vue';
 
 </script>
 
